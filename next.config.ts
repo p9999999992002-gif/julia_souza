0de998e9ea675dcfa6ebcee0676bdfb1d0f7a4ee
p9999999992002-gif <p9999999992002@gmail.com>
2026-05-14 19:07:35 -0300

    add para subir site

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
 import type { NextConfig } from "next";
 
+const repoName = "/NOME_DO_REPO";
+
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "export",
+  basePath: repoName,
+  images: { unoptimized: true },
 };
 
 export default nextConfig;
