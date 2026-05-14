9f95088a1760979ab5ebbfeaaf7ce7fa754b7ad6
p9999999992002-gif <p9999999992002@gmail.com>
2026-05-14 19:53:03 -0300

    teste v1

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  turbopack: {
-    root: __dirname,
+  output: "export",
+  basePath: "/julia_souza",
+  assetPrefix: "/julia_souza/",
+  images: {
+    unoptimized: true,
   },
 };
 
