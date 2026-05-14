a35ca333ff9485e8cf965cc511db25f7e55acc6f
p9999999992002-gif <p9999999992002@gmail.com>
2026-05-14 19:14:59 -0300

    teste publicar

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,7 @@
 import type { NextConfig } from "next";
 
-const repoName = "/NOME_DO_REPO";
-
 const nextConfig: NextConfig = {
   output: "export",
-  basePath: repoName,
   images: { unoptimized: true },
 };
 
