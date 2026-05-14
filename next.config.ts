53706def48fa5e5827520009e8da0419e8fd584b
p9999999992002-gif <p9999999992002@gmail.com>
2026-05-14 19:26:37 -0300

    1234213 123

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
 import type { NextConfig } from "next";
 
+const repo = "/julia_souza";
+
 const nextConfig: NextConfig = {
   output: "export",
+  basePath: repo,
+  assetPrefix: repo + "/",
   images: {
     unoptimized: true,
   },
